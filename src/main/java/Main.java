3f2163045ff3af802679d8e77867948df59ed817
Luis5Felipe <luisfelipedos29@gmail.com>
2025-04-16 20:34:50 -0300

    feat: teste de responta adicionando

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,23 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.botconfiguration.ReadyListener;
+import org.botconfiguration.commands.Ping;
 import org.botconfiguration.token.Token;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         JDA jda = JDABuilder.createDefault(Token.get())
-                .addEventListeners(new ReadyListener())
+                .enableIntents(
+                        GatewayIntent.GUILD_MESSAGES,
+                        GatewayIntent.MESSAGE_CONTENT,
+                        GatewayIntent.GUILD_MEMBERS
+                )
+                .setActivity(Activity.playing("/help"))
+                .addEventListeners(new ReadyListener(), new Ping())
                 .build();
         jda.awaitReady();
+
     }
 }
